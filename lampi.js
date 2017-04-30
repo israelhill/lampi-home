ce878288546966519c533389f24aa3d4c1e7fafe
israelhill <idh@case.edu>
2017-04-30 02:33:23 +0000

    sending messages is working

--- lampi.js
+++ lampi.js
@@ -10,14 +10,8 @@ var CERT = fs.readFileSync('/etc/keys/client.crt');
 var CAfile = fs.readFileSync('/etc/keys/ca.crt');
 
 
-// var websocketOptions = {host:'mqtts://ec2-52-11-88-168.us-west-2.compute.amazonaws.com', port:5002};
-// var options = {clientId: 'israel_nodejs', protocolId: 'MQIsdp', protocolVersion: 3, connectTimeout:1000,
-//     debug:true, };
-
-// var options = {clientId: 'israel_nodejs', protocolId: 'MQIsdp', protocolVersion: 3, connectTimeout:1000,
-//     debug:true, rejectUnauthorized: false, host: 'ec2-52-11-88-168.us-west-2.compute.amazonaws.com', port:50002};
-
-var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8);
+// var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8);
+var clientId = '0.7678878316189812_web_client';
 var options = {
     keepalive: 10,
     clientId: clientId,
@@ -35,12 +29,13 @@ var options = {
 
 var client = mqtt.connect(host, options);
 var lampState = {
-    color : {
-        h: "50",
-        s: "50"
+    'color' : {
+        'h': 0.99,
+        's': 0.99
     },
-    brightness : "50",
-    on: true
+    'brightness' : 0.99,
+    'on': true
+
 };
 
 
@@ -54,7 +49,6 @@ client.on('connect', function (err) {
 
 client.on('message', function (topic, message) {
     console.log('MQTT: Received a message');
-    // message is Buffer
     console.log('MQTT Message: ', message.toString());
 });
 
@@ -64,17 +58,7 @@ client.on('error', function(message){
 });
 
 exports.sendMessage = function(state) {
-    client.publish(setConfigTopic, state);
+    var msg = JSON.stringify(lampState);
+    client.publish(setConfigTopic, msg, {qos: 2});
 }
 
-function sendTestMessage() {
-    console.log("Publishing a message");
-    client.publish(setConfigTopic, lampState, { qos: 0, retain: false });
-}
-
-
-
-
-// var state = {};
-// const topic = "/devices/" + deviceId + "/lamp/set_config";
-// client.publish(topic, state);
